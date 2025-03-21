96b4e22816aa32bba7a8bbe192688969b63947c4
9bfish8 <9bfish8@gmail.com>
2025-03-21 20:22:34 +0900

    build(main): 빌드 시 ESLint 오류 무시 설정 추가 - 빌드 시 ESLint 오류를 무시하도록 설정을 추가합니다.

--- next.config.js
+++ next.config.js
@@ -10,6 +10,10 @@ const nextConfig = {
     compiler: {
         styledComponents: true,
     },
+    // 빌드 시 ESLint 오류 무시 설정 추가
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 module.exports = withPWA(nextConfig);
