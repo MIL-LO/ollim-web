b3c177e50a2e63f49bafa469d340c4e17a3823fc
9bfish8 <9bfish8@gmail.com>
2025-03-21 22:30:59 +0900

    build(dev): Sentry 설정 업데이트 및 테스트 기능 추가
    - package.json에 새로운 dev 및 start 스크립트 추가
    - .env.sentry-build-plugin 파일 .gitignore에 추가
    - page.tsx에 Sentry 오류 및 메시지 테스트 기능 추가
    - sentry.server.config.ts, sentry.client.config.ts, sentry.edge.config.ts 추가
    - next.config.js에 Sentry 구성 설정 추가
    - global-error.tsx 및 instrumentation.ts 파일 추가

--- next.config.js
+++ next.config.js
@@ -1,19 +1,43 @@
+const { withSentryConfig } = require('@sentry/nextjs');
 const withPWA = require('next-pwa')({
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    compiler: {
-        styledComponents: true,
-    },
-    // 빌드 시 ESLint 오류 무시 설정 추가
-    eslint: {
-        ignoreDuringBuilds: true,
-    },
+  reactStrictMode: true,
+  compiler: {
+    styledComponents: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
-module.exports = withPWA(nextConfig);
+// PWA 구성으로 Next.js 구성 래핑
+const configWithPWA = withPWA(nextConfig);
+
+// Sentry 구성으로 Next.js 구성 래핑
+module.exports = withSentryConfig(
+  configWithPWA,
+  {
+    // Sentry 웹팩 플러그인 옵션
+    org: "millo-j0",
+    project: "ollim-web-dev",
+
+    // 소스맵 업로드 설정
+    widenClientFileUpload: true,
+
+    // 광고 차단기 우회를 위한 라우트 설정
+    tunnelRoute: "/monitoring",
+
+    // Sentry 로그 설정
+    silent: !process.env.CI,
+
+    // 그 외 옵션들
+    disableLogger: true,
+    automaticVercelMonitors: true,
+  }
+);
