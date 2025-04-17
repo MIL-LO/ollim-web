4469bc899b74a61cee769a9d95159049dd924c0b
9bfish8 <9bfish8@gmail.com>
2025-04-17 21:03:05 +0900

    fix : 예시 인디케이터 삭제

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,7 +13,6 @@ export default function LoginPage() {
         <ButtonsWrapper>
           <LoginButtons />
         </ButtonsWrapper>
-        <BottomIndicator />
       </LoginContainer>
     </MobileLayout>
   );
